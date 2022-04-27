8159182c40a3f6a0e77d46e8fedcbfafd60897ed
Charan Gowda <101180515+Gowdacharan@users.noreply.github.com>
2022-04-27 06:43:54 +0000

    added problem number 9

--- script.js
+++ script.js
@@ -49,6 +49,87 @@ for (let i = 1; i <= number; i++) {
 console.log(sum);
 
 
+//problem 8
+
+ function removeSpaces(str) {
+        
+        var count = 0;
+ 
+        
+        for (var i = 0; i < str.length; i++)
+          if (str[i] !== " ") str[count++] = str[i];
+         
+ 
+        return count;
+      }
+ 
+      
+      var str = "LoremIpsum".split("");
+      var i = removeSpaces(str);
+      document.write(str.join("").substring(0, i));
+      let length = str.length;
+      console.log(str.length)
+
+
+
+//problem 9
+
+
+let root1, root2;
+
+
+let a = 1;
+let b = 5;
+let c = 6;
+
+
+let discriminant = b * b - 4 * a * c;
+
+
+if (discriminant > 0) {
+    root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+    
+    console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+}
+
+
+else if (discriminant == 0) {
+    root1 = root2 = -b / (2 * a);
+
+    
+    console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+}
+
+
+else {
+    let realPart = (-b / (2 * a)).toFixed(2);
+    let imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
+
+    
+    console.log(
+    `The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`
+  );
+}
+
+
+
+
+ 
+
+
+
+
+
+
+
+ 
+  
+  
+ 
+
+
 
    
 
